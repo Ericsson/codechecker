fc6f85fd5a4a07fdaa370ccb75330a503bc8d1bc
Gyorgy Orban <gyorgy.orban@ericsson.com>
2015-08-24 14:06:05 +0200

    update loglevels in case there is a db version mismatch

--- viewer_server/client_db_access_handler.py
+++ viewer_server/client_db_access_handler.py
@@ -136,10 +136,10 @@ class ThriftRequestHandler():
             version_from_db = 'v'+str(version.major)+'.'+str(version.minor)
             if db_version_info.is_compatible(version.major,
                                              version.minor):
-                LOG.debug('Version mismatch. Expected database version: '
+                LOG.error('Version mismatch. Expected database version: '
                           + str(db_version_info))
-                LOG.debug('Version from the database is: ' + version_from_db)
-                LOG.debug('Please update your database.')
+                LOG.error('Version from the database is: ' + version_from_db)
+                LOG.error('Please update your database.')
                 sys.exit(1)
         else:
             LOG.debug('No version information found in the database.')
