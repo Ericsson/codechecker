479359e2326838a3697ed48aa3c9358e9bd2777f
Gyorgy Orban <gyorgy.orban@ericsson.com>
2015-10-13 23:32:28 +0200

    fix regex and suppress info removal from file also add additional comments

--- codechecker_lib/suppress_file_handler.py
+++ codechecker_lib/suppress_file_handler.py
@@ -6,10 +6,10 @@
 ''' suppress file format
 
 # this is the old format
-# 123324353456463442341242342343#1 || bug hash comment
+123324353456463442341242342343#1 || bug hash comment
 
 # this is the new format
-# 123324353456463442341242342343#1 || filename || bug hash comment
+123324353456463442341242342343#1 || filename || bug hash comment
 
 '''
 
@@ -31,10 +31,10 @@ def get_suppress_data(suppress_file):
     processs a file object for suppress information
     """
 
-    old_format_pattern = r"^(?P<bug_hash>[\d\w]{32})\#(?P<bug_hash_type>\d)\s*\|\|\s*(?P<comment>[^\|]*)"
+    old_format_pattern = r"^(?P<bug_hash>[\d\w]{32})\#(?P<bug_hash_type>\d)\s*\|\|\s*(?P<comment>[^\|]*)$"
     old_format = re.compile(old_format_pattern, re.UNICODE)
 
-    new_format_pattern = r"^(?P<bug_hash>[\d\w]{32})\#(?P<bug_hash_type>\d)\s*\|\|\s*(?P<file_name>[^\\]+)\s*\|\|\s*(?P<comment>.*)"
+    new_format_pattern = r"^(?P<bug_hash>[\d\w]{32})\#(?P<bug_hash_type>\d)\s*\|\|\s*(?P<file_name>[^\\\|]+)\s*\|\|\s*(?P<comment>[^\|]*)$"
     new_format = re.compile(new_format_pattern, re.UNICODE)
 
     suppress_data = []
@@ -63,7 +63,7 @@ def get_suppress_data(suppress_file):
                                   old_format_match['comment']))
             continue
 
-        if line != '':
+        if line.strip() != '':
             LOG.warning('Malformed suppress line: ' + line)
 
     return suppress_data
@@ -104,6 +104,10 @@ def write_to_suppress_file(suppress_file, value, hash_type, file_name, comment='
 
 
 def remove_from_suppress_file(suppress_file, value, hash_type, file_name):
+    """
+    remove suppress information from the suppress file
+    old and new format is supported
+    """
 
     LOG.debug('Removing ' + value + ' from \n' + suppress_file)
 
@@ -111,12 +115,29 @@ def remove_from_suppress_file(suppress_file, value, hash_type, file_name):
         s_file = codecs.open(suppress_file, 'r+', 'UTF-8')
         lines = s_file.readlines()
 
-        lines = filter(lambda line: not line.startswith(value +
-                                                        HASH_TYPE_SEPARATOR +
-                                                        str(hash_type) +
-                                                        COMMENT_SEPARATOR +
-                                                        file_name),
-                                                        lines)
+        # filter out new format first because it is more specific
+        old_format_pattern = r"^" + value + r"\#" + str(hash_type) + \
+            r"\s*\|\|\s*(?P<comment>[^\|]*)$"
+        old_format = re.compile(old_format_pattern, re.UNICODE)
+
+        new_format_pattern = r"^" + value + r"\#" + str(hash_type) + \
+            r"\s*\|\|\s*" + file_name + r"\s*\|\|\s*(?P<comment>[^\|]*)$"
+        new_format = re.compile(new_format_pattern, re.UNICODE)
+
+        def check_for_match(line):
+            """
+            check if the line matches the new or old format
+            """
+            line = line.strip()
+            if re.match(new_format, line.strip()):
+                return False
+            if re.match(old_format, line.strip()):
+                return False
+            else:
+                return True
+
+        # filter out lines which should be removed
+        lines = filter(lambda line: check_for_match(line), lines)
 
         s_file.seek(0)
         s_file.truncate()
