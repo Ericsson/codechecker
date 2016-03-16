bb8b4a12d01787391dbd1af71a09b02971cfc1d6
Boldizsar Toth <boldizsar.toth@ericsson.com>
2016-03-16 12:02:42 +0100

    Travis test script parameters put into a shell script as env variables.

---
+++ test_env_setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export TEST_CODECHECKER_PACKAGE_DIR="./codechecker_package"
+export TEST_CODECHECKER_DIR="$TEST_CODECHECKER_PACKAGE_DIR/CodeChecker"
+export TEST_CLANG_VERSION="stable"
+export TEST_DBPORT="5432"
+export TEST_DBUSERNAME="postgres"
