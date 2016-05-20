18cea7c22806021d141598e1a9fbd10fd04916d4
Gyorgy Orban <gyorgy.orban@ericsson.com>
2016-05-20 09:33:58 +0200

    added cleanup for the temporary test package and documentation update

--- tests/test_env_setup.sh
+++ tests/test_env_setup.sh
@@ -4,6 +4,7 @@ export TEST_TESTS_DIR=$(readlink -f "$(dirname "$BASH_SOURCE")")
 
 export TEST_CODECHECKER_PACKAGE_DIR=`mktemp -d`
 export TEST_CODECHECKER_DIR="$TEST_CODECHECKER_PACKAGE_DIR/CodeChecker"
+echo "Temporary directory for testing created: " $TEST_CODECHECKER_PACKAGE_DIR
 export TEST_CLANG_VERSION="stable"
 
 export TEST_TEST_PROJECT_CONFIG="$TEST_TESTS_DIR/test_projects/test_files/project_info.json"
