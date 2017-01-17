c05577ffed2ed04b8659b872036399fae7426f7f
bruntib <bruntib@gmail.com>
2017-01-17 18:16:11 +0100

    Bugfix: crash in plist parser.
    Fixes #497

--- codechecker_lib/arg_handler.py
+++ codechecker_lib/arg_handler.py
@@ -339,7 +339,7 @@ def consume_plist(item):
     rh.buildaction.analyzer_type = 'Build action from plist'
     rh.buildaction.original_command = plist
     rh.analyzer_cmd = ''
-    rh.analyzer.analyzed_source_file = ''  # TODO: fill from plist.
+    rh.analyzed_source_file = ''  # TODO: fill from plist.
     rh.result_file = os.path.join(args.directory, plist)
     rh.handle_results()
 
