1052bb8a6d22983c08af78a3ffa1d5073617844f
Daniel Krupp <daniel.krupp@ericsson.com>
2017-07-03 15:39:34 +0200

    store analyzer failures into separate files

---
+++ tests/functional/analyze/__init__.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+# -----------------------------------------------------------------------------
+#                     The CodeChecker Infrastructure
+#   This file is distributed under the University of Illinois Open Source
+#   License. See LICENSE.TXT for details.
+# -----------------------------------------------------------------------------
+
+"""Setup for the test package analyze.
+"""
+
+import os
+import shutil
+
+from libtest import codechecker
+from libtest import env
+from libtest import get_free_port
+from libtest import project
+
+
+# Test workspace should be initialized in this module.
+TEST_WORKSPACE = None
+
+
+def setup_package():
+    """Setup the environment for the tests."""
+
+    global TEST_WORKSPACE
+    TEST_WORKSPACE = env.get_workspace('analyze')
+
+    os.environ['TEST_WORKSPACE'] = TEST_WORKSPACE
+
+
+def teardown_package():
+    """Delete the workspace associated with this test"""
+
+    # TODO: If environment variable is set keep the workspace
+    # and print out the path.
+    global TEST_WORKSPACE
+
+    print("Removing: " + TEST_WORKSPACE)
+    shutil.rmtree(TEST_WORKSPACE)
---
+++ tests/functional/analyze/test_files/failure.c
@@ -0,0 +1,4 @@
+int main(void){
+	return 0;
+	xxx //will cause a compilation error
+}
