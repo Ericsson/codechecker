bca710d77bebc804659699c730016be09eece57a
Gyorgy Orban <gyorgy.orban@ericsson.com>
2017-08-03 16:56:35 +0200

    show filter in the url

--- www/scripts/codecheckerviewer/filterHelper.js
+++ www/scripts/codecheckerviewer/filterHelper.js
@@ -47,26 +47,35 @@ function (declare, Button, ContentPane, Filter) {
    * callback function which is passed an object that describes the values of
    * a Select dojo object.
    *
-   * The function can be called with 3 or 4 arguments. In case of 3 arguments
+   * The function can be called with 4 or 5 arguments. In case of 4 arguments
    * a Filter object, a run, and the callback function has to be passed. In case
-   * of 4 arguments two runs has to be passed in which case the diff result
+   * of 5 arguments two runs has to be passed in which case the diff result
    * types is computed.
    */
   function checkerInfoOptions() {
-    if (arguments.length === 3) {
+    if (arguments.length === 4) {
       var filter = arguments[0];
-      var run1 = arguments[1];
-      var cb = arguments[2];
+      var filterValues = arguments[1];
+      var run1 = arguments[2];
+      var cb = arguments[3];
     } else {
       var filter = arguments[0];
-      var run1 = arguments[1];
-      var run2 = arguments[2];
-      var cb = arguments[3];
+      var filterValues = arguments[1];
+      var run1 = arguments[2];
+      var run2 = arguments[3];
+      var cb = arguments[4];
+    }
+
+    var checkerToSelect = 'all';
+    if( filterValues ){
+      checkerToSelect = filterValues.checker;
+      // query all but only select which was on the filter url!!!!!
+      filterValues.checker = '';
     }
 
     function result(runResultTypes) {
-      var checkerInfo = parseReportDataTypeCounts(runResultTypes);
 
+      var checkerInfo = parseReportDataTypeCounts(runResultTypes);
       var selectOptions = [{
         label : checkerInfo.Infinity.name
           + ' (' + checkerInfo.Infinity.count + ')',
@@ -80,86 +89,118 @@ function (declare, Button, ContentPane, Filter) {
       severities.sort(function (a, b) { return b.value - a.value; })
       .forEach(function (severity) {
         var item = checkerInfo[severity.value];
-
         if (item.count > 0) {
           selectOptions.push({
             label : item.name + ' (' + item.count + ')',
-            value : 'severity#' + severity.value
+            value : 'severity#' + severity.value,
+            selected : 'severity#' + severity.value === checkerToSelect
           });
 
-          for (var checkerKey in item.checkers)
+          for (var checkerKey in item.checkers){
+            var val = 'checker#' + checkerKey;
+            var sel = val === checkerToSelect;
             selectOptions.push({
               label : '&nbsp;&nbsp;&nbsp;&nbsp;' + checkerKey
                 + ' (' + item.checkers[checkerKey] + ')',
-              value : 'checker#' + checkerKey
+              value : val,
+              selected : sel
             });
+          }
+
         }
       });
 
       cb(selectOptions);
     }
 
-    var reportFilter = module.filterToReportFilter(filter);
+    // Construct a filter to get the number of results for the
+    // checker select part of the filter on the UI.
+    var reportFilter = module.filterToReportFilter(filter, filterValues);
 
-    var difftype;
-    switch (filter.getValue('difftype')) {
-      case 'new':        difftype = CC_OBJECTS.DiffType.NEW;        break;
-      case 'resolved':   difftype = CC_OBJECTS.DiffType.RESOLVED;   break;
-      case 'unresolved': difftype = CC_OBJECTS.DiffType.UNRESOLVED; break;
+    if (arguments.length === 4) {
+      CC_SERVICE.getRunResultTypes( run1, [reportFilter], result);
     }
+    else {
 
-    if (arguments.length === 3) {
-      CC_SERVICE.getRunResultTypes(
-        run1,
-        [reportFilter],
-        result);
+      // Compare UI values to the provided filter values, if they are different
+      // use the UI values to filter.
+      // UI values might be empty on page load!
+      var uiValues = filter.getValues()
+      var difftype = filterValues ? filterValues.difftype : 'new';
+
+      if (uiValues.difftype !== difftype && uiValues.difftype) {
+        difftype = uiValues.difftype;
+      }
+
+      switch (difftype) {
+        case 'new':        difftype = CC_OBJECTS.DiffType.NEW;        break;
+        case 'resolved':   difftype = CC_OBJECTS.DiffType.RESOLVED;   break;
+        case 'unresolved': difftype = CC_OBJECTS.DiffType.UNRESOLVED; break;
+      }
+
+      CC_SERVICE.getDiffResultTypes( run1, run2, difftype, [reportFilter],
+                                     result);
     }
-    else
-      CC_SERVICE.getDiffResultTypes(
-        run1,
-        run2,
-        difftype,
-        [reportFilter],
-        result);
   }
 
-  function addPathFilterField(filter) {
-    filter.addField('text', 'path', 'Path filter');
+  /**
+   * Add path field and set to the value.
+   */
+  function addPathFilterField(filter, value) {
+    filter.addField('text', 'path', 'Path filter', value);
   }
 
-  function addCheckerIndexField(filter, run1, run2) {
+  /**
+   * Register the callbacks for the checker filter fields.
+   */
+  function addCheckerIndexField(filter, filterValues, run1, run2) {
     var callback
-      = arguments.length === 2
-      ? function (cb) { return checkerInfoOptions(filter, run1, cb); }
-      : function (cb) { return checkerInfoOptions(filter, run1, run2, cb); }
-
+      = arguments.length === 3
+      ? function (cb) { return checkerInfoOptions(filter, filterValues, run1, cb); }
+      : function (cb) { return checkerInfoOptions(filter, filterValues, run1, run2, cb); }
     filter.addField('select', 'checker', callback);
   }
 
-  function addDiffTypeField(filter) {
-    filter.addField('select', 'difftype', [
+  /**
+   * Set the diff type filter field.
+   */
+  function addDiffTypeField(filter, selected_value) {
+    var options = [
       { label : 'New only',   value : 'new'        },
-      { label : 'Resolved',   value : 'resolved'   },
-      { label : 'Unresolved', value : 'unresolved' }
-    ]);
+      { label : 'Resolved',   value : 'resolved', selected: 'resolved' === selected_value },
+      { label : 'Unresolved', value : 'unresolved', selected: 'unresolved' === selected_value}
+    ];
+
+    filter.addField('select', 'difftype', options);
   }
 
+  /**
+   * Create a filter group which can contain multiple filters.
+   */
   var FilterGroup = declare(ContentPane, {
-    constructor : function () {
+    constructor : function (filters) {
       this.style = 'padding: 5px';
     },
 
     postCreate : function () {
-      this._addPlusButton(this._addFilter());
+        filters = this.filters;
+        if ( !filters ) {
+          this._addPlusButton(this._addFilter());
+        } else {
+          this._addPlusButton(this._addFilter(filters[0]));
+          for (i = 1; i < filters.length; i++) {
+            this._addMinusButton(this._addFilter(filters[i]));
+          }
+        }
     },
 
-    _addFilter : function () {
+    _addFilter : function (filterValue) {
       var that = this;
 
       var filter
         = this.runId
-        ? module.createOverviewFilter(this.runId)
-        : module.createDiffOverviewFilter(this.baselineId, this.newcheckId);
+        ? module.createOverviewFilter(this.runId, filterValue)
+        : module.createDiffOverviewFilter(this.baselineId, this.newcheckId, filterValue);
 
       filter.onChange = function () {
         var allValues = [];
@@ -204,22 +245,40 @@ function (declare, Button, ContentPane, Filter) {
   });
 
   var module = {
-    createOverviewFilter : function (runId) {
+    /**
+     * Construct and initialize a filter.
+     */
+    createOverviewFilter : function (runId, filterValue) {
       var filter = new Filter();
-
-      addPathFilterField(filter);
-      addCheckerIndexField(filter, runId);
-
+      if ( filterValue === undefined ){
+        addPathFilterField(filter, '');
+        // Needs all the filter values to calculate the checker filter field.
+        addCheckerIndexField(filter, filterValue, runId);
+      } else {
+        addPathFilterField(filter, filterValue.path);
+        // Needs all the filter values to calculate the checker filter field.
+        addCheckerIndexField(filter, filterValue, runId);
+      }
       return filter;
     },
 
-    createDiffOverviewFilter : function (baselineId, newcheckId) {
+    /**
+     * Construct and initialize a diff filter.
+     */
+    createDiffOverviewFilter : function (baselineId, newcheckId, filterValue) {
       filter = new Filter();
 
-      addPathFilterField(filter);
-      addDiffTypeField(filter);
-      addCheckerIndexField(filter, baselineId, newcheckId);
-
+      if ( !filterValue ){
+        addPathFilterField(filter, '');
+        addDiffTypeField(filter);
+        // Needs all the filter values to calculate the checker filter field.
+        addCheckerIndexField(filter, filterValue, baselineId, newcheckId);
+      } else {
+        addPathFilterField(filter, filterValue.path);
+        addDiffTypeField(filter, filterValue.difftype);
+        // Needs all the filter values to calculate the checker filter field.
+        addCheckerIndexField(filter, filterValue, baselineId, newcheckId);
+      }
       return filter;
     },
 
@@ -227,33 +286,61 @@ function (declare, Button, ContentPane, Filter) {
      * This function converts an object of type Filter (created earlier by
      * createOverviewFilter()) to ReportFilter thrift object.
      */
-    filterToReportFilter : function (filter) {
+    filterToReportFilter : function (filter, fv) {
       var reportFilter = new CC_OBJECTS.ReportFilter();
-      var filterValues = filter.getValues();
-      
-      if (!filterValues.checker)
-        filterValues.checker = '';
 
-      reportFilter.filepath = '*' + filterValues.path + '*';
+      if ( !fv ){
+        // Setup default values for the report filter.
+        reportFilter.filepath = '**';
+        reportFilter.suppressed = false;
+        reportFilter.checkerId = null;
+        reportFilter.severity = null;
+      }
+      else {
+        var uiValues = filter.getValues();
 
-      var index = filterValues.checker.indexOf('#');
-      var type = filterValues.checker.substr(0, index);
-      var value = filterValues.checker.substr(index + 1);
+        // Compare UI values to the provided filter values, if they are different
+        // use the UI values to filter.
+        // UI values might be empty on page load!
+        if (uiValues.suppression === fv.suppression || !uiValues.suppression) {
+          reportFilter.suppressed = fv.suppression === 'supp';
+        } else {
+          reportFilter.suppressed = uiValues.suppression === 'supp';
+        }
 
-      switch (type) {
-        case 'severity': reportFilter.severity = parseInt(value); break;
-        case 'checker': reportFilter.checkerId = value; break;
+        reportFilter.filepath = '*' +
+          (uiValues.filepath === fv.filepath ? fv.path : uiValues.path) + '*';
+
+        if (fv.checker === 'all'){
+          reportFilter.checkerId = null;
+        } else {
+          // The filter checker value is not all it can be a severity level
+          // or a concrete checker.
+          var index = fv.checker.indexOf('#');
+          var type = fv.checker.substr(0, index);
+          var value = fv.checker.substr(index + 1);
+
+          switch (type) {
+            case 'severity': reportFilter.severity = parseInt(value); break;
+            case 'checker': reportFilter.checkerId = value; break;
+          }
+        }
       }
 
       return reportFilter;
     },
 
-    filterGroupToReportFilters : function (filterGroup) {
+    filterGroupToReportFilters : function (filterGroup, filters) {
       var that = this;
 
       var allReportFilters = [];
-      filterGroup.getChildren().forEach(function (filter) {
-        allReportFilters.push(that.filterToReportFilter(filter));
+      filterGroup.getChildren().forEach(function (filter, i) {
+        if( !filters){
+          allReportFilters.push(that.filterToReportFilter(filter));
+        }
+        else{
+          allReportFilters.push(that.filterToReportFilter(filter, filters[i]));
+        }
       });
 
       return allReportFilters;
