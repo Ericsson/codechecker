ba57014f0d41f8d9f1d306af92a8e1fb432b9966
bruntib <bruntib@gmail.com>
2017-10-17 17:21:46 +0200

    Refuse uploading too big .zip files
    Fixes #840

--- libcodechecker/util.py
+++ libcodechecker/util.py
@@ -300,3 +300,16 @@ def arg_match(options, args):
             continue
 
     return matched_args
+
+
+def sizeof_fmt(num, suffix='B'):
+    """
+    Pretty print storage units.
+    Source: https://stackoverflow.com/questions/1094841/
+        reusable-library-to-get-human-readable-version-of-file-size
+    """
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
