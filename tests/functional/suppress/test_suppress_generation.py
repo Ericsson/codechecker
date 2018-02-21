655a2132900732166442f1e8fb1fac0aa6d028b9
Gyorgy Orban <gyorgy.orban@ericsson.com>
2018-02-21 16:39:41 +0100

    factor out process call and fix returncode in suppress test

--- tests/functional/suppress/test_suppress_generation.py
+++ tests/functional/suppress/test_suppress_generation.py
@@ -21,6 +21,24 @@ from libtest import codechecker
 from libtest.thrift_client_to_db import get_all_run_results
 
 
+def call_cmd(command, cwd, env):
+    try:
+        print(' '.join(command))
+        proc = subprocess.Popen(
+            shlex.split(' '.join(command)),
+            cwd=cwd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            env=env)
+        out, err = proc.communicate()
+        print(out)
+        print(err)
+        return proc.returncode
+    except CalledProcessError as cerr:
+        print("Failed to call:\n" + ' '.join(cerr.cmd))
+        return cerr.returncode
+
+
 class TestSuppress(unittest.TestCase):
     """
     Test source-code level suppression data writing to suppress file.
@@ -54,23 +72,6 @@ class TestSuppress(unittest.TestCase):
         Test exporting a source suppress comment automatically to file.
         """
 
-        def __call(command):
-            try:
-                print(' '.join(command))
-                proc = subprocess.Popen(
-                    shlex.split(' '.join(command)),
-                    cwd=self._test_project_path,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                    env=env.test_env(self._test_workspace))
-                out, err = proc.communicate()
-                print(out)
-                print(err)
-                return 0
-            except CalledProcessError as cerr:
-                print("Failed to call:\n" + ' '.join(cerr.cmd))
-                return cerr.returncode
-
         generated_file = os.path.join(self._test_workspace,
                                       "generated.suppress")
 
@@ -80,7 +81,9 @@ class TestSuppress(unittest.TestCase):
                        "--export-source-suppress"
                        ]
 
-        ret = __call(extract_cmd)
+        ret = call_cmd(extract_cmd,
+                       self._test_project_path,
+                       env.test_env(self._test_workspace))
         self.assertEqual(ret, 0, "Failed to generate suppress file.")
 
         with open(generated_file, 'r') as generated:
