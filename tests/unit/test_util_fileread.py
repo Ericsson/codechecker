ed55895ecde1420b6d1e1cde405d8847ab24e47e
Gyorgy Orban <gyorgy.orban@ericsson.com>
2018-06-05 14:07:23 +0200

    use io.open to open files to get source lines
    Lines with carriage return character in a unix encoded file
    could cause to read the wrong line from the file.
    Using io.open where universal newlines are enabled by default
    handles this correctly.

---
+++ tests/unit/test_util_fileread.py
@@ -0,0 +1,39 @@
+# -----------------------------------------------------------------------------
+#                     The CodeChecker Infrastructure
+#   This file is distributed under the University of Illinois Open Source
+#   License. See LICENSE.TXT for details.
+# -----------------------------------------------------------------------------
+
+import os
+import unittest
+
+from libcodechecker.util import get_line
+
+
+class GetLineTest(unittest.TestCase):
+    """
+    Tests to get source file lines.
+    """
+
+    def test_util_getline(self):
+        """
+        Lines in files with carriage return character
+        should be handled as separate lines.
+        """
+        test_file_path = os.path.dirname(os.path.realpath(__file__))
+        file_to_process = os.path.join(test_file_path, 'newline')
+
+        line1 = get_line(file_to_process, 1)
+        self.assertEqual(line1, 'line1\n')
+
+        line2 = get_line(file_to_process, 2)
+        self.assertEqual(line2, 'line2\n')
+
+        line4 = get_line(file_to_process, 4)
+        self.assertEqual(line4, 'line4\n')
+
+        line5 = get_line(file_to_process, 5)
+        self.assertEqual(line5, 'line5\n')
+
+        line6 = get_line(file_to_process, 6)
+        self.assertEqual(line6, 'line6\n')
