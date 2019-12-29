ccda13faa0818a3da0b26629c6f4775a9fe7d5f0
Márton Csordás <csordasmarton92@gmail.com>
2019-12-29 15:03:37 +0100

    [vue] Setup Webpack with Babel

--- web/server/vue-cli/webpack.config.js
+++ web/server/vue-cli/webpack.config.js
@@ -20,6 +20,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader'
       },
