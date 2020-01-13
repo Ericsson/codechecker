5deed2d9d03422a29a85558b559742c3c60675d4
Márton Csordás <csordasmarton92@gmail.com>
2020-01-13 15:16:35 +0100

    [vue] Init severity and detection status filter items

--- web/server/vue-cli/src/components/ReportFilter/Filters/DetectionStatusFilter.vue
+++ web/server/vue-cli/src/components/ReportFilter/Filters/DetectionStatusFilter.vue
@@ -1,10 +1,59 @@
 <template>
-  <h3>Detection status</h3>
+  <select-option
+    title="Detection status"
+    :items="items"
+    :fetch-items="fetchItems"
+    :loading="loading"
+  >
+    <template v-slot:icon="{ item }">
+      <detection-status-icon :status="item.id" />
+    </template>
+  </select-option>
 </template>
 
 <script>
+import { ccService } from '@cc-api';
+
+import { DetectionStatus } from "@cc/report-server-types";
+import { DetectionStatusIcon } from "@/components/icons";
+
+import SelectOption from './SelectOption/SelectOption';
 
 export default {
-  name: 'DetectionStatusFilter'
+  name: 'DetectionStatusFilter',
+  components: {
+    SelectOption,
+    DetectionStatusIcon
+  },
+  data() {
+    return {
+      selected: [],
+      items: [],
+      loading: false
+    };
+  },
+
+  methods: {
+    fetchItems() {
+      this.loading = true;
+
+      const runIds = null;
+      const reportFilter = null;
+      const cmpData = null;
+
+      ccService.getClient().getDetectionStatusCounts(runIds, reportFilter,
+      cmpData, (err, res) => {
+        this.items = Object.keys(DetectionStatus).map(status => {
+          const id = DetectionStatus[status];
+          return {
+            id: id,
+            title: status,
+            count: res[id] !== undefined ? res[id] : 0
+          };
+        });
+        this.loading = false;
+      });
+    }
+  }
 }
 </script>
--- web/server/vue-cli/src/components/ReportFilter/Filters/SeverityFilter.vue
+++ web/server/vue-cli/src/components/ReportFilter/Filters/SeverityFilter.vue
@@ -1,10 +1,59 @@
 <template>
-  <h3>Severity</h3>
+  <select-option
+    title="Severity"
+    :items="items"
+    :fetch-items="fetchItems"
+    :loading="loading"
+  >
+    <template v-slot:icon="{ item }">
+      <severity-icon :status="item.id" />
+    </template>
+  </select-option>
 </template>
 
 <script>
+import { ccService } from '@cc-api';
+
+import { Severity } from "@cc/report-server-types";
+import { SeverityIcon } from "@/components/icons";
+
+import SelectOption from './SelectOption/SelectOption';
 
 export default {
-  name: 'SeverityFilter'
+  name: 'SeverityFilter',
+  components: {
+    SelectOption,
+    SeverityIcon
+  },
+  data() {
+    return {
+      selected: [],
+      items: [],
+      loading: false
+    };
+  },
+
+  methods: {
+    fetchItems() {
+      this.loading = true;
+
+      const runIds = null;
+      const reportFilter = null;
+      const cmpData = null;
+
+      ccService.getClient().getSeverityCounts(runIds, reportFilter, cmpData,
+      (err, res) => {
+        this.items = Object.keys(Severity).map(status => {
+          const severityId = Severity[status];
+          return {
+            id: severityId,
+            title: status,
+            count: res[severityId] !== undefined ? res[severityId] : 0
+          };
+        });
+        this.loading = false;
+      });
+    }
+  }
 }
 </script>
