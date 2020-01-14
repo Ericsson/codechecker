12c9bad5d4f486c27b88e2080eb0fc6735a66511
Márton Csordás <csordasmarton92@gmail.com>
2020-01-14 14:54:21 +0100

    [vue] Helper tooltip for unique filter

--- web/server/vue-cli/src/components/ReportFilter/Filters/UniqueFilter.vue
+++ web/server/vue-cli/src/components/ReportFilter/Filters/UniqueFilter.vue
@@ -1,17 +1,39 @@
 <template>
   <v-checkbox
     v-model="reportFilter.isUnique"
-    :label="'Unique reports'"
-  />
+  >
+    <template v-slot:label>
+      Unique reports
+      <v-tooltip max-width="200" right>
+        <template v-slot:activator="{ on }">
+          <v-icon
+            color="accent"
+            class="ml-1"
+            v-on="on"
+          >
+            mdi-help-circle
+          </v-icon>
+        </template>
+        <span>
+          This narrows the report list to unique bug. The same bug may appear
+          several times if it is found on different control paths, i.e. through
+          different function calls. By checking <b>Unique reports</b> a report
+          appears only once even if it is found on several paths.
+        </span>
+      </v-tooltip>
+    </template>
+  </v-checkbox>
 </template>
 
 <script>
 import VCheckbox from "Vuetify/VCheckbox/VCheckbox";
+import VTooltip from "Vuetify/VTooltip/VTooltip";
+import VIcon from "Vuetify/VIcon/VIcon";
 
 export default {
   name: 'UniqueFilter',
   components: {
-    VCheckbox
+    VCheckbox, VTooltip, VIcon
   },
   props: {
     reportFilter: { type: Object, required: true }
