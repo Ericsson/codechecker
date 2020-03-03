eb1763f7ec25a8137b8f0fdc86bd01ed83ba956a
Márton Csordás <csordasmarton92@gmail.com>
2020-03-03 16:12:56 +0100

    [vue] Fix eslint errors in tests

--- web/server/vue-cli/src/mixins/__tests__/detection-status.mixin.test.js
+++ web/server/vue-cli/src/mixins/__tests__/detection-status.mixin.test.js
@@ -6,8 +6,8 @@ const toString =
 const fromString =
   DetectionStatusMixin.methods.detectionStatusFromStringToCode;
 
-describe('Convert detection status', () => {
-  test('Convert existing detection statuses to string', () => {
+describe("Convert detection status", () => {
+  test("Convert existing detection statuses to string", () => {
     expect(toString(DetectionStatus.NEW)).toEqual("New");
     expect(toString(DetectionStatus.RESOLVED)).toEqual("Resolved");
     expect(toString(DetectionStatus.UNRESOLVED)).toEqual("Unresolved");
@@ -16,12 +16,12 @@ describe('Convert detection status', () => {
     expect(toString(DetectionStatus.UNAVAILABLE)).toEqual("Unavailable");
   });
 
-  test('Convert non existing detection statuses to string', () => {
+  test("Convert non existing detection statuses to string", () => {
     expect(toString(-1)).toEqual("");
     expect(toString(null)).toEqual("");
   });
 
-  test('Convert string to detection status', () => {
+  test("Convert string to detection status", () => {
     expect(fromString("New")).toEqual(DetectionStatus.NEW);
     expect(fromString("ResOlved")).toEqual(DetectionStatus.RESOLVED);
     expect(fromString("UNRESOLVED")).toEqual(DetectionStatus.UNRESOLVED);
@@ -30,7 +30,7 @@ describe('Convert detection status', () => {
     expect(fromString("Unavailable")).toEqual(DetectionStatus.UNAVAILABLE);
   });
 
-  test('Convert string to non existing detection status', () => {
+  test("Convert string to non existing detection status", () => {
     expect(fromString("")).toEqual(-1);
     expect(fromString("dummy")).toEqual(-1);
   });
