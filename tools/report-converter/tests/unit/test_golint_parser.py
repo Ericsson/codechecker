f9521522046dd5a685923951a7c967f62ebcd8b2
Márton Csordás <csordasmarton92@gmail.com>
2020-03-19 14:56:40 +0100

    [report-converter][test] Add Golint tests

---
+++ tools/report-converter/tests/unit/test_golint_parser.py
@@ -0,0 +1,73 @@
+# -----------------------------------------------------------------------------
+#                     The CodeChecker Infrastructure
+#   This file is distributed under the University of Illinois Open Source
+#   License. See LICENSE.TXT for details.
+# -----------------------------------------------------------------------------
+
+"""
+This module tests the correctness of the GolintAnalyzerResult, which
+used in sequence transform Golint output to a plist file.
+"""
+
+
+import os
+import plistlib
+import shutil
+import tempfile
+import unittest
+
+from codechecker_report_converter.golint.analyzer_result import \
+    GolintAnalyzerResult
+
+
+class GolintAnalyzerResultTestCase(unittest.TestCase):
+    """ Test the output of the GolintAnalyzerResult. """
+
+    def setUp(self):
+        """ Setup the test. """
+        self.analyzer_result = GolintAnalyzerResult()
+        self.cc_result_dir = tempfile.mkdtemp()
+        self.test_files = os.path.join(os.path.dirname(__file__),
+                                       'golint_output_test_files')
+
+    def tearDown(self):
+        """ Clean temporary directory. """
+        shutil.rmtree(self.cc_result_dir)
+
+    def test_no_go_output_file(self):
+        """ Test transforming single go file. """
+        analyzer_result = os.path.join(self.test_files, 'files',
+                                       'simple.go')
+
+        ret = self.analyzer_result.transform(analyzer_result,
+                                             self.cc_result_dir)
+        self.assertFalse(ret)
+
+    def test_transform_dir(self):
+        """ Test transforming single plist file. """
+        analyzer_result = os.path.join(self.test_files)
+
+        ret = self.analyzer_result.transform(analyzer_result,
+                                             self.cc_result_dir)
+        self.assertFalse(ret)
+
+    def test_transform_single_file(self):
+        """ Test transforming single plist file. """
+        analyzer_result = os.path.join(self.test_files, 'simple.out')
+        self.analyzer_result.transform(analyzer_result, self.cc_result_dir)
+
+        plist_file = os.path.join(self.cc_result_dir,
+                                  'simple.go_golint.plist')
+
+        with open(plist_file, mode='rb') as pfile:
+            res = plistlib.load(pfile)
+
+            # Use relative path for this test.
+            res['files'][0] = os.path.join('files', 'simple.go')
+
+        plist_file = os.path.join(self.test_files,
+                                  'simple.expected.plist')
+        with open(plist_file, mode='rb') as pfile:
+            exp = plistlib.load(pfile)
+
+        self.assertEqual(res, exp)
