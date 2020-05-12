2f18cfaebaa5710d5fb9d4e0270bfa28082bf71e
Márton Csordás <csordasmarton92@gmail.com>
2020-05-12 20:32:31 +0200

    [version] Upgrade codechecker api to 6.27.0

--- web/codechecker_web/shared/version.py
+++ web/codechecker_web/shared/version.py
@@ -16,7 +16,7 @@ SESSION_COOKIE_NAME = '__ccPrivilegedAccessToken'
 # The newest supported minor version (value) for each supported major version
 # (key) in this particular build.
 SUPPORTED_VERSIONS = {
-    6: 26
+    6: 27
 }
 
 # Used by the client to automatically identify the latest major and minor
