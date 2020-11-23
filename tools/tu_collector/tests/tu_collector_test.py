9c9aa3cc8968f0b85111e4c594d8dabaf57bd96b
bruntib <bruntib@gmail.com>
2020-11-23 16:34:07 +0100

    [tu-collector] Collect CTU-loaded source files
    CodeChecker generates files under report directory which list which other
    source files were involved in a CTU analysis. tu_collector needs to collect
    these files too, so it has been extended with --ctu-deps-dir flag that can be
    given this generated folder.

--- tools/tu_collector/tests/tu_collector_test.py
+++ tools/tu_collector/tests/tu_collector_test.py
@@ -13,8 +13,10 @@ These files are compressed in a .zip file. This test intends to check if the
 """
 
 
+import inspect
 import json
 import os
+import shutil
 import tempfile
 import unittest
 import zipfile
@@ -28,23 +30,21 @@ class TUCollectorTest(unittest.TestCase):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         self._test_proj_dir = os.path.join(dir_path, 'project')
 
-    def test_file_existence(self):
-
         compile_json = os.path.join(self._test_proj_dir,
                                     'compile_command.json')
 
-        compile_cmd_data = {}
         with open(compile_json, mode='rb') as cmpjson:
-            compile_cmd_data = json.load(cmpjson)
+            self.compile_cmd_data = json.load(cmpjson)
 
         # Overwrite the directory paths.
         # This is needed because the tests run on different machines
         # so the directory path changes in each case.
-        for cmp in compile_cmd_data:
+        for cmp in self.compile_cmd_data:
             cmp['directory'] = self._test_proj_dir
 
+    def test_file_existence(self):
         zip_file_name = tempfile.mkstemp(suffix='.zip')[1]
-        tu_collector.zip_tu_files(zip_file_name, compile_cmd_data)
+        tu_collector.zip_tu_files(zip_file_name, self.compile_cmd_data)
 
         with zipfile.ZipFile(zip_file_name) as archive:
             files = archive.namelist()
@@ -60,3 +60,32 @@ class TUCollectorTest(unittest.TestCase):
         self.assertTrue(
             any([path.endswith(os.path.join('/', 'hello.c')) for path in files]))
         self.assertIn('compilation_database.json', files)
+
+    def test_ctu_collection(self):
+        with tempfile.TemporaryDirectory() as ctu_deps_dir:
+            ctu_action = next(filter(lambda ba: ba['file'] == 'ctu.cpp',
+                                     self.compile_cmd_data))
+
+            hash_fun = dict(
+                inspect.getmembers(tu_collector))['__analyzer_action_hash']
+
+            with open(os.path.join(ctu_deps_dir, hash_fun(ctu_action)), 'w') \
+                    as f:
+                f.write(os.path.join(self._test_proj_dir, 'zero.cpp'))
+
+            with tempfile.NamedTemporaryFile(suffix='.zip') as zip_file:
+                tu_collector.zip_tu_files(zip_file.name, self.compile_cmd_data,
+                                          file_filter='ctu.cpp',
+                                          ctu_deps_dir=ctu_deps_dir)
+
+                with zipfile.ZipFile(zip_file.name) as archive:
+                    files = archive.namelist()
+
+        self.assertTrue(
+            any([path.endswith(os.path.join('/', 'vector')) for path in files]))
+        self.assertTrue(
+            any([path.endswith(os.path.join('/', 'ctu.cpp')) for path in files]))
+        self.assertTrue(
+            any([path.endswith(os.path.join('/', 'zero.cpp')) for path in files]))
+        self.assertTrue(
+            any([path.endswith(os.path.join('/', 'zero.h')) for path in files]))
