29ef2b6ce7df0a4d4e13e4268c49dc226b0cb164
Márton Csordás <csordasmarton92@gmail.com>
2021-06-08 08:56:53 +0200

    [server] Unzip storage zip file to workspace directory
    Previously the storage zip file is unzipped to the `/tmp` folder. It is
    possible that the disk size of the host machine is not so big and when
    the unzipped / decompressed zip size is too big that the user will
    get `No space left on device` exception.
    
    For this reason we will unzip the zip file to the workspace folder which
    can be configured during a server start and it can point to an extra
    network drive which size can be bigger than the host machine drive.
    
    This patch also removes our `TemporaryDirectory` class because it is
    available in the `tempfile` module since `Python 3.2`.

--- web/server/codechecker_server/tmp.py
+++ web/server/codechecker_server/tmp.py
@@ -13,37 +13,11 @@ Temporary directory module.
 import datetime
 import hashlib
 import os
-import shutil
-import tempfile
 
 
 from codechecker_common.logger import get_logger
 
 LOG = get_logger('system')
-
-
-class TemporaryDirectory:
-    def __init__(self, suffix='', prefix='tmp', tmp_dir=None):
-        self._closed = False
-        self.name = tempfile.mkdtemp(suffix, prefix, tmp_dir)
-
-    def __enter__(self):
-        return self.name
-
-    def __cleanup(self):
-        if self.name and not self._closed:
-            try:
-                shutil.rmtree(self.name)
-            except (TypeError, AttributeError) as ex:
-                print("ERROR: {0} while cleaning up {1}".format(ex, self.name))
-                return
-            self._closed = True
-
-    def __exit__(self, *args):
-        self.__cleanup()
-
-    def __del__(self, *args):
-        self.__cleanup()
 
 
 def get_tmp_dir_hash():
