6b0ec0655784d3339e58e7f8dbfb60e71b7157a9
Márton Csordás <csordasmarton92@gmail.com>
2021-06-15 14:14:27 +0200

    [docker] Support running docker image with different users

--- web/docker/entrypoint.sh
+++ web/docker/entrypoint.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-if [ "$(id -u)" == '0' ]; then
-  # Set PostgreSQL password file from secrets.
-  pgpass=/run/secrets/pgpass
-  if [ -f $pgpass ]; then
-    cp $pgpass /.pgpass
-    chmod 0600 /.pgpass
-    chown codechecker:codechecker /.pgpass
-    export PGPASSFILE=/.pgpass
-  fi
+USER_ID=$($USER)
+USER_GROUP="$(id -g)"
+WORKSPACE_DIR="/workspace"
+PGPASS_FILE=$WORKSPACE_DIR/.pgpass
 
-  # Change the owner of the workspace directory
-  mkdir -p /workspace
-  chown codechecker:codechecker /workspace
+if [ "$(id -u)" = '0' ]; then
+  # Create workspace directory and change the permission to 'codechecker' user
+  # if this directory doesn't exists.
+  if [ ! -d $WORKSPACE_DIR ]; then
+    mkdir -p $WORKSPACE_DIR
+    chown -R codechecker:codechecker $WORKSPACE_DIR
+  fi
 
   # Execute this script again with codechecker user.
   exec gosu codechecker "$0" "$@"
+fi
+
+# Set PostgreSQL password file from secrets.
+pgpass=/run/secrets/pgpass
+if [ -f $pgpass ]; then
+  cat $pgpass > ${PGPASS_FILE}
+  chmod 0600 ${PGPASS_FILE}
+  chown ${USER_ID}:${USER_GROUP} ${PGPASS_FILE}
+  export PGPASSFILE=${PGPASS_FILE}
 fi
 
 exec "$@"
