a45167cd6084cc2e8ba71add843177f0f57eef11
Márton Csordás <csordasmarton92@gmail.com>
2021-12-16 10:48:30 +0100

    [cli] Fix JSON format of `CodeChecker version` subcommand
    The output of the `CodeChecker version -o json` command wasn't a valid
    JSON format. With this patch we will provide a valid JSON output for this
    command.

--- analyzer/codechecker_analyzer/cmd/analyzer_version.py
+++ analyzer/codechecker_analyzer/cmd/analyzer_version.py
@@ -8,17 +8,54 @@
 """
 Defines a subcommand for CodeChecker which prints version information.
 """
-
-
 import argparse
 import json
 
+from typing import Dict, List, Tuple
 from codechecker_analyzer import analyzer_context
 
 from codechecker_report_converter import twodim
 
 from codechecker_common import logger
 from codechecker_common.output import USER_FORMATS
+
+
+LOG = logger.get_logger('system')
+
+
+class Version:
+    def __init__(self):
+        context = analyzer_context.get_context()
+
+        self.version = context.version
+        self.build_date = context.package_build_date
+        self.git_hash = context.package_git_hash
+        self.git_tag = context.package_git_tag
+
+    def to_dict(self) -> Dict[str, str]:
+        """ Get version information in dictionary format. """
+        return {
+            "base_package_version": self.version,
+            "package_build_date": self.build_date,
+            "git_commit": self.git_hash,
+            "git_tag": self.git_tag}
+
+    def to_list(self) -> List[Tuple[str, str]]:
+        """ Get version information in list format. """
+        return [
+            ("Base package version", self.version),
+            ("Package build date", self.build_date),
+            ("Git commit ID (hash)", self.git_hash),
+            ("Git tag information", self.git_tag)]
+
+    def print(self, output_format: str):
+        """ Print analyzer version information in the given format. """
+        if output_format == "json":
+            print(json.dumps(self.to_dict()))
+        else:
+            LOG.info("CodeChecker analyzer version:")
+            print(twodim.to_str(
+                output_format, ["Kind", "Version"], self.to_list()))
 
 
 def get_argparser_ctor_args():
@@ -58,29 +95,6 @@ def add_arguments_to_parser(parser):
     parser.set_defaults(func=main)
 
 
-def print_version(output_format=None):
-    """
-    Print analyzer version information in the given format.
-    """
-    context = analyzer_context.get_context()
-
-    rows = [
-        ("Base package version", context.version),
-        ("Package build date", context.package_build_date),
-        ("Git commit ID (hash)", context.package_git_hash),
-        ("Git tag information", context.package_git_tag)
-    ]
-
-    if output_format == "json":
-        # Use a special JSON format here, instead of
-        # [ {"kind": "something", "version": "0.0.0"}, {"kind": "foo", ... } ]
-        # do
-        # { "something": "0.0.0", "foo": ... }
-        print(json.dumps(dict(rows)))
-    else:
-        print(twodim.to_str(output_format, ["Kind", "Version"], rows))
-
-
 def main(args):
     """
     Get and print the version information from the version config
@@ -94,4 +108,4 @@ def main(args):
 
     logger.setup_logger(args.verbose if 'verbose' in args else None, stream)
 
-    print_version(args.output_format)
+    Version().print(args.output_format)
