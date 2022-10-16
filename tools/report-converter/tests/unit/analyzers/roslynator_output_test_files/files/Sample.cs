fb851b7a619b0b62bfc0edb8db1bd09f6075694f
Péter Kaszab <27868208+PeterKaszab@users.noreply.github.com>
2022-10-16 15:31:55 +0200

    Improve unit tests

--- tools/report-converter/tests/unit/analyzers/roslynator_output_test_files/files/Sample.cs
+++ tools/report-converter/tests/unit/analyzers/roslynator_output_test_files/files/Sample.cs
@@ -1,10 +1,19 @@
-﻿namespace files;
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace files;
 
 public class Sample
 {
+    // Triggers CA1822
     public int Add(int a, int b)
     {
-        int x;
         return a + b;
+    }
+
+    // Triggers CA1806
+    public void AddOne(IEnumerable<int> enumerable)
+    {
+        enumerable.Select(n => Add(1, n));
     }
 }
