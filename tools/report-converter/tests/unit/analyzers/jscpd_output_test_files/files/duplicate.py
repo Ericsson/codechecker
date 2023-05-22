ffaf68e0eb62408517df762acc74066c338401d4
Duc Nguyen <Duc@MacBook-Pro-cua-Nguyen.local>
2023-05-22 15:38:43 +0200

    Add a new report converter to translate jscpd output into .plist format for integration with CodeChecker

---
+++ tools/report-converter/tests/unit/analyzers/jscpd_output_test_files/files/duplicate.py
@@ -0,0 +1,23 @@
+def greet_and_add1():
+    print("Hello, User! How are you doing today?")
+    a = 5
+    b = 7
+    result = a + b
+    print(f"The sum of {a} and {b} is: {result}")
+
+
+def greet_and_add2():
+    print("Hello, User! How are you doing today?")
+    a = 5
+    b = 7
+    result = a + b
+    print(f"The sum of {a} and {b} is: {result}")
+
+
+def main():
+    greet_and_add1()
+    greet_and_add2()
+
+
+if __name__ == "__main__":
+    main()
