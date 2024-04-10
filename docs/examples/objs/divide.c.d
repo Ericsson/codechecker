533ec2ee9f4a955135cb49ef84880846152cf69e
Samat Tamerlan <vez21p@inf.elte.hu>
2024-04-11 01:09:42 +0200

    fb infer support

---
+++ docs/examples/objs/divide.c.d
@@ -0,0 +1,4 @@
+/home/t/codechecker/docs/examples/objs/divide.c.o: \
+ /home/t/codechecker/docs/examples/src/divide.c \
+ /home/t/codechecker/docs/examples/incl/divide.h
+/home/t/codechecker/docs/examples/incl/divide.h:
---
+++ docs/examples/objs/main.c.d
@@ -0,0 +1,4 @@
+/home/t/codechecker/docs/examples/objs/main.c.o: \
+ /home/t/codechecker/docs/examples/src/main.c \
+ /home/t/codechecker/docs/examples/incl/divide.h
+/home/t/codechecker/docs/examples/incl/divide.h:
