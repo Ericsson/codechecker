cf0d76ee18601bcf8ee9d2510673e01686397a19
Samat Tamerlan <vez21p@inf.elte.hu>
2024-06-28 12:40:13 +0200

    unnecessary files have been removed

--- docs/examples/objs/divide.c.d
+++
@@ -1,4 +0,0 @@
-/home/t/codechecker/docs/examples/objs/divide.c.o: \
- /home/t/codechecker/docs/examples/src/divide.c \
- /home/t/codechecker/docs/examples/incl/divide.h
-/home/t/codechecker/docs/examples/incl/divide.h:
