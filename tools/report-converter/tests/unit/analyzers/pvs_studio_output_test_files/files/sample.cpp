fcb4f4155ee6fa15d2c7b8e395ab505026ba6b0a
Valerii Filatov <valiklfiatoff@yandex.ru>
2024-09-30 16:14:08 +0300

    PVS-Studio report's tests

---
+++ tools/report-converter/tests/unit/analyzers/pvs_studio_output_test_files/files/sample.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+
+int main()
+{
+  int a = 5;
+	int b = 6;
+
+	if (a < b) {
+		return 1;
+	}
+
+	return 0;
+}
---
+++ tools/report-converter/tests/unit/analyzers/test_pvs_studio_parser.py
@@ -0,0 +1,63 @@
+import unittest
+import tempfile
+import shutil
+import plistlib
+import os
+
+
+from codechecker_report_converter.analyzers.pvs_studio import analyzer_result
+
+class PvsStudioAnalyzerResultTestCase(unittest.TestCase):
+    """ Test the output of PVS-Studio's AnalyzerResult. """
+
+    def setUp(self):
+        """ Set up the test. """
+        self.analyzer_result = analyzer_result.AnalyzerResult()
+        self.result_dir = tempfile.mkdtemp()
+        self.test_files = os.path.join(os.path.dirname(__file__),
+                                       'pvs_studio_output_test_files')
+
+    def tearDown(self):
+        """Clean temporary directory. """
+        shutil.rmtree(self.result_dir)
+
+    def test_no_report_output_file(self):
+        """ Test transforming single cpp file. """
+        result = os.path.join(self.test_files, "files", "sample.cpp")
+
+        is_success = self.analyzer_result.transform([result], self.result_dir, file_name="{source_file}_{analyzer}")
+
+        self.assertFalse(is_success)
+
+    def test_transform_dir(self):
+        """ Test transforming a directory. """
+        result = os.path.join(self.test_files)
+
+        is_success = self.analyzer_result.transform(
+            [analyzer_result], self.result_dir, file_name="{source_file}_{analyzer}")
+        
+        self.assertFalse(is_success)
+
+    def test_transform_single_file(self):
+        """ Test transforming single output file. """
+        analyzer_result = os.path.join(self.test_files, 'sample.out')
+        self.analyzer_result.transform(
+            [analyzer_result], self.result_dir,
+            file_name="{source_file}_{analyzer}")
+
+        plist_file = os.path.join(self.result_dir,
+                                  'sample.plist')
+
+        with open(plist_file, mode='rb') as pfile:
+            res = plistlib.load(pfile)
+
+            # Use relative path for this test.
+            res['files'][0] = os.path.join('files', 'sample.cpp')
+
+        plist_file = os.path.join(self.test_files,
+                                  'sample.plist')
+
+        with open(plist_file, mode='rb') as pfile:
+            exp = plistlib.load(pfile)
+
+        self.assertEqual(res, exp)
