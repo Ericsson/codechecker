d5c25a611ec3d04bed6e6b44a001d7ec5f57c1ac
Kristóf Umann <dkszelethus@gmail.com>
2025-05-07 17:06:36 +0200

    hack path back in

--- analyzer/tests/functional/codechecker_bazel/test_bazel.py
+++ analyzer/tests/functional/codechecker_bazel/test_bazel.py
@@ -46,12 +46,6 @@ class TestBaseTest(unittest.TestCase):
         # Move to test dir
         cls.test_dir = os.path.abspath(os.path.dirname(__file__))
         os.chdir(cls.test_dir)
-        print(f"test interpreter: {sys.executable}")
-        print("%%%%%%%%%%%%%")
-        print("%%%%%%%%%%%%%")
-        print(sys.path)
-        print("%%%%%%%%%%%%%")
-        print("%%%%%%%%%%%%%")
 
     @classmethod
     def tearDownClass(cls):
