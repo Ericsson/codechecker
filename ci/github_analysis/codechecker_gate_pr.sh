a7aadc9314f51f5aa047abae0c1d0ebd982e504c
Gergely Gulyas <gergely.gulyas.ext@ericsson.com>
2025-07-21 14:26:37 +0200

    [fix] Remove unnecessary branch name when from gating script invocation

--- ci/github_analysis/codechecker_gate_pr.sh
+++ ci/github_analysis/codechecker_gate_pr.sh
@@ -2,10 +2,6 @@
 
 CC_URL="https://codechecker-demo.eastus.cloudapp.azure.com/codechecker"
 
-if [ "$#" -ne 1 ]; then
-    echo "<PR_NAME> is missing"
-fi
-
 ./ci/github_analysis/pylint_analyze.sh
 report-converter -c -t pylint -o ./reports-pylint ./pylint-reports.json
 CodeChecker cmd diff --url "$CC_URL" -b master -n ./reports-pylint --new
