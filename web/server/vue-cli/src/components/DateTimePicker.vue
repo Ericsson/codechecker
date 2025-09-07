c4998998af2324bf3791c51d4a4a003d5c3e8178
xb058t <xb058t@inf.elte.hu>
2025-09-07 21:29:10 +0200

    Added runs page

--- web/server/vue-cli/src/components/DateTimePicker.vue
+++ web/server/vue-cli/src/components/DateTimePicker.vue
@@ -4,17 +4,16 @@
     :content-class="dialogClass"
     width="400"
   >
-    <template v-slot:activator="{ on }">
+    <template #activator="{ props }">
       <v-text-field
+        v-bind="props"
         :label="label"
-        :value="formattedDatetime"
-        :class="[ inputClass, 'pa-0', 'ma-0' ]"
+        :class="[inputClass, 'pa-0', 'ma-0']"
         :prepend-inner-icon="prependInnerIcon"
         :outlined="outlined"
-        :dense="dense"
+        :density="dense ? 'compact' : undefined"
         hide-details
         readonly
-        v-on="on"
       >
         <template #append>
           <slot name="append" />
@@ -24,58 +23,51 @@
 
     <v-card>
       <v-card-text class="pa-0">
-        <v-tabs
-          v-model="activeTab"
-          fixed-tabs
-        >
+        <v-tabs v-model="activeTab" grow>
           <v-tab>
             <v-icon>mdi-calendar</v-icon>
           </v-tab>
-
-          <v-tab
-            :disabled="!date"
-          >
+          <v-tab :disabled="!date">
             <v-icon>mdi-clock-outline</v-icon>
           </v-tab>
+        </v-tabs>
 
-          <v-tab-item>
+        <v-window v-model="activeTab">
+          <v-window-item>
             <v-date-picker
               v-model="date"
-              full-width
-              @input="activeTab = 1"
+              show-adjacent-months
+              color="primary"
+              @update:model-value="activeTab = 1"
             />
-          </v-tab-item>
-
-          <v-tab-item>
+          </v-window-item>
+          <v-window-item>
             <v-time-picker
               v-model="time"
-              full-width
-              use-seconds
               format="24hr"
+              full-width
+              with-seconds
+              color="primary"
             />
-          </v-tab-item>
-        </v-tabs>
+          </v-window-item>
+        </v-window>
       </v-card-text>
 
       <v-card-actions>
         <v-spacer />
-
         <v-btn
           color="grey lighten-1"
-          class="clear-btn"
-          text
-          @click.native="clear"
+          variant="text"
+          @click="clear"
         >
           Clear
         </v-btn>
-
         <v-btn
-          class="ok-btn"
           color="green darken-1"
-          text
+          variant="text"
           @click="ok"
         >
-          Ok
+          OK
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -88,7 +80,7 @@ import { format, parse } from "date-fns";
 export default {
   name: "DateTimePicker",
   props: {
-    value: { type: [ Date, String ], default: null },
+    value: { type: [Date, String], default: null },
     label: { type: String, default: "" },
     dateFormat: { type: String, default: "yyyy-MM-dd" },
     timeFormat: { type: String, default: "HH:mm:ss" },
@@ -97,7 +89,7 @@ export default {
     dialogClass: { type: String, default: null },
     outlined: { type: Boolean, default: false },
     dense: { type: Boolean, default: false },
-    prependInnerIcon: { type: String, default: null },
+    prependInnerIcon: { type: String, default: null }
   },
   data() {
     return {
@@ -107,36 +99,31 @@ export default {
       time: this.defaultTime
     };
   },
-
   computed: {
     dateTimeFormat() {
       return `${this.dateFormat} ${this.timeFormat}`;
     },
-
     dateTime() {
-      if (this.date && this.time) {
-        const dt = this.date + " " + this.time;
-        return parse(dt, this.dateTimeFormat, new Date());
-      }
 
-      return null;
+      let hours, minutes;
+      [hours,minutes] = this.time.split(":");
+      const formatted = new Date(this.date);
+      formatted.setHours(parseInt(hours));
+      formatted.setMinutes(parseInt(minutes));
+      return formatted;
     },
-
     formattedDatetime() {
       return this.dateTime ? format(this.dateTime, this.dateTimeFormat) : null;
     }
   },
-
   watch: {
-    value() {
-      this.init();
+    value: {
+      handler() {
+        this.init();
+      },
+      immediate: true
     }
   },
-
-  mounted() {
-    this.init();
-  },
-
   methods: {
     init() {
       if (!this.value) {
@@ -147,34 +134,34 @@ export default {
       let initValue = null;
       if (this.value instanceof Date) {
         initValue = this.value;
-      } else if (typeof this.value === "string" ||
-                 this.value instanceof String
-      ) {
-        initValue = parse(this.value, this.dateTimeFormat, new Date());
+      } else if (typeof this.value === "string") {
+        try {
+          initValue = parse(this.value, this.dateTimeFormat, new Date());
+        } catch (e) {
+          initValue = null;
+        }
       }
 
-      this.date = format(initValue, this.dateFormat);
-      this.time = format(initValue, this.timeFormat);
+      if (initValue) {
+        this.date = format(initValue, this.dateFormat);
+        this.time = format(initValue, this.timeFormat);
+      } else {
+        this.resetDateTimes();
+      }
     },
-
     clear() {
       this.reset();
       this.resetDateTimes();
-
       this.$emit("input", null);
     },
-
     ok() {
       this.reset();
-
       this.$emit("input", this.dateTime);
     },
-
     reset() {
       this.dialog = false;
       this.activeTab = 0;
     },
-
     resetDateTimes() {
       this.date = null;
       this.time = this.defaultTime;
@@ -183,13 +170,8 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-::v-deep .v-picker.v-card {
+<style scoped>
+.v-picker {
   box-shadow: none;
-
-  & > .v-picker__title {
-    border-radius: 0;
-  }
 }
-
 </style>
--- web/server/vue-cli/src/router/index.js
+++ web/server/vue-cli/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
   {
@@ -126,7 +126,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 });
 
