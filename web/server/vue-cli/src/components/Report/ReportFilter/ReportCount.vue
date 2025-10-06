e0ba6221d24f76fc3e73c85bc961f01188946a93
Gergely Gulyas <gergely.gulyas.ext@ericsson.com>
2025-10-06 13:30:54 +0200

    Add hover text to outstanding reports numbers on filters sidebar.
    The report number next to the filters was just there without any
    explanation. This commit adds a tooltip which shows what the number shows.

--- web/server/vue-cli/src/components/Report/ReportFilter/ReportCount.vue
+++ web/server/vue-cli/src/components/Report/ReportFilter/ReportCount.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="pa-1 text-no-wrap">
+  <span class="pa-1 text-no-wrap" title="Number of reports">
     <v-icon :color="'#ec7672'">mdi-bug</v-icon>
     <b>{{ value }}</b>
   </span>
