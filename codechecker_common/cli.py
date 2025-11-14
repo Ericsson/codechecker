476252380ae7e097ff3d61b51d01bc6974b74f82
Hassan Afifi <hassan.m.afify@gmail.com>
2025-11-15 00:53:45 +0100

    Improve argcomplete support

--- codechecker_common/cli.py
+++ codechecker_common/cli.py
@@ -71,12 +71,8 @@ def patch_argcomplete_parser():
     def parse_known_args_compat(self, arg_strings, namespace, *args, **kwargs):
         return original(self, arg_strings, namespace)
 
-<<<<<<< HEAD
-    ac_argparse.IntrospectiveArgumentParser._parse_known_args = parse_known_args_compat
-=======
     ac_argparse.IntrospectiveArgumentParser._parse_known_args = \
         parse_known_args_compat
->>>>>>> b526b75a (Improve argcomplete support)
 
 
 def get_data_files_dir_path():
@@ -214,11 +210,7 @@ output.
                           subcommand + "'... ignoring.")
                     import traceback
                     traceback.print_exc(file=sys.stdout)
-<<<<<<< HEAD
-
-=======
         
->>>>>>> b526b75a (Improve argcomplete support)
         patch_argcomplete_parser()
         argcomplete.autocomplete(parser)
         args = parser.parse_args()
