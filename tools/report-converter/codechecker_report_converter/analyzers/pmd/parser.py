0d53bcddc7f4ae77b2cc0b761f16c2c9533c9d45
Marton Robert Zsolt <ay3vbl@inf.elte.hu>
2026-01-20 12:59:04 +0100

    Fixed missing license PMD analyzer

--- tools/report-converter/codechecker_report_converter/analyzers/pmd/parser.py
+++ tools/report-converter/codechecker_report_converter/analyzers/pmd/parser.py
@@ -1,3 +1,11 @@
+# -------------------------------------------------------------------------
+#
+#  Part of the CodeChecker project, under the Apache License v2.0 with
+#  LLVM Exceptions. See LICENSE for license information.
+#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# -------------------------------------------------------------------------
+
 import json
 import os
 from typing import List
