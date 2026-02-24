e5a77a8c733d8c7fbcd0138805477d2d360699c7
Endre Fülöp <endre.fulop@sigmatechnology.com>
2026-02-24 15:47:55 +0100

    Simplify migration by removing dialect branching

--- web/server/codechecker_server/migrations/report/versions/a1b2c3d4e5f6_duration_millisecond_precision.py
+++ web/server/codechecker_server/migrations/report/versions/a1b2c3d4e5f6_duration_millisecond_precision.py
@@ -7,7 +7,6 @@ Create Date: 2026-02-24 13:14:00.000000
 """
 
 from alembic import op
-import sqlalchemy as sa
 
 
 revision = 'a1b2c3d4e5f6'
@@ -17,36 +16,16 @@ depends_on = None
 
 
 def upgrade():
-    connection = op.get_bind()
-    dialect = connection.dialect.name
-
-    if dialect == 'postgresql':
-        op.execute("""
-            UPDATE runs
-            SET duration = duration * 1000
-            WHERE duration != -1
-        """)
-    elif dialect == 'sqlite':
-        op.execute("""
-            UPDATE runs
-            SET duration = duration * 1000
-            WHERE duration != -1
-        """)
+    op.execute("""
+        UPDATE runs
+        SET duration = duration * 1000
+        WHERE duration != -1
+    """)
 
 
 def downgrade():
-    connection = op.get_bind()
-    dialect = connection.dialect.name
-
-    if dialect == 'postgresql':
-        op.execute("""
-            UPDATE runs
-            SET duration = duration / 1000
-            WHERE duration != -1
-        """)
-    elif dialect == 'sqlite':
-        op.execute("""
-            UPDATE runs
-            SET duration = duration / 1000
-            WHERE duration != -1
-        """)
+    op.execute("""
+        UPDATE runs
+        SET duration = duration / 1000
+        WHERE duration != -1
+    """)
