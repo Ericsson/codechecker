acea2efb600b1f11f29b3665a38c90ae26e96f81
Gergely Gulyas <gergely.gulyas.ext@ericsson.com>
2026-05-27 12:10:24 +0200

    [gui] Replace v-dialog to custom component in statistics dialog
    Previously a simple v-dialog was used in StatisticsDialog. This change
    replaces it with a custom built dialog with in built closing button,
    title handling.

--- web/server/vue-cli/src/components/Statistics/StatisticsDialog.vue
+++ web/server/vue-cli/src/components/Statistics/StatisticsDialog.vue
@@ -1,75 +1,58 @@
 <template>
-  <v-dialog
+  <ConfirmDialog
     v-model="dialog"
-    content-class="documentation-dialog"
     max-width="70%"
-    scrollable
+    :title="title"
+    :buttons="false"
   >
-    <v-card>
-      <v-card-title
-        class="headline primary white--text"
-        primary-title
+    <template v-slot:content>
+      <div 
+        v-for="run in runs"
+        :key="run.message"
       >
-        {{ title }}
-
-        <v-spacer />
-
-        <v-btn
-          class="close-btn"
-          icon="mdi-close"
-          @click="dialog = false"
-        />
-      </v-card-title>
-
-      <v-card-text>
-        <div 
-          v-for="run in runs"
-          :key="run.message"
-        >
-          <div class="font-alert-message">
-            <span 
-              v-if="run.message && run.runNames.length"
-            >
-              {{ run.runNames.length > 1 ? "These runs are": "This run is" }}
-              {{ run.message }}
-            </span>
-          </div>
-          <v-container class="checker-rows-in-columns">
-            <v-row
-              v-for="runName in run.runNames"
-              :key="runName"
-              no-gutters
-              align="center"
-            >
-              <v-col cols="auto" align-self="center">
-                <v-icon 
-                  v-if="type == 'enabled'"
-                  class="mr-1"
-                  color="success"
-                >
-                  mdi-check 
-                </v-icon>
-                <v-icon 
-                  v-else
-                  class="mr-1"
-                  color="error"
-                >
-                  mdi-close
-                </v-icon>
-              </v-col>
-              <v-col
-                col="auto"
-                align-self="center" 
-                style="font-size: larger;"
-              >
-                {{ runName }}
-              </v-col>
-            </v-row>
-          </v-container>
+        <div class="font-alert-message">
+          <span 
+            v-if="run.message && run.runNames.length"
+          >
+            {{ run.runNames.length > 1 ? "These runs are": "This run is" }}
+            {{ run.message }}
+          </span>
         </div>
-      </v-card-text>
-    </v-card>
-  </v-dialog>
+        <v-container class="checker-rows-in-columns">
+          <v-row
+            v-for="runName in run.runNames"
+            :key="runName"
+            no-gutters
+            align="center"
+          >
+            <v-col cols="auto" align-self="center">
+              <v-icon 
+                v-if="type == 'enabled'"
+                class="mr-1"
+                color="success"
+              >
+                mdi-check 
+              </v-icon>
+              <v-icon 
+                v-else
+                class="mr-1"
+                color="error"
+              >
+                mdi-close
+              </v-icon>
+            </v-col>
+            <v-col
+              col="auto"
+              align-self="center" 
+              style="font-size: larger;"
+            >
+              {{ runName }}
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
+    </template>
+  </ConfirmDialog>
 </template>
 
 <script setup>
@@ -78,23 +61,20 @@ import {
   CheckerInfoAvailability,
   setCheckerStatusUnavailableDueToVersion,
 } from "@/composables/useAnalysisInfo";
+import ConfirmDialog from "@/components/ConfirmDialog";
 
 const props = defineProps({
-  value: { type: Boolean, required: true },
+  modelValue: { type: Boolean, required: true },
   checkerName: { type: String, default: null },
   type: { type: String, required: true },
   runData: { type: Array, default: () => [] }
 });
 
-const emit = defineEmits([ "update:value" ]);
+const emit = defineEmits([ "update:modelValue" ]);
 
 const dialog = computed({
-  get() {
-    return props.value;
-  },
-  set(val) {
-    emit("update:value", val);
-  }
+  get: () => props.modelValue,
+  set: val => emit("update:modelValue", val)
 });
 
 const title = computed(function() {
