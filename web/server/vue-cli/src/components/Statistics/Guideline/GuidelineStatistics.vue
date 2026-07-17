c76001bff67be066e828bbcd40b4797aeb8257ef
Gergely Gulyas <gergely.gulyas.ext@ericsson.com>
2026-07-17 13:18:13 +0200

    [gui] Fix guideline statistics not getting the report filter set
    This changes fixes a missing .value to set the report filters right

--- web/server/vue-cli/src/components/Statistics/Guideline/GuidelineStatistics.vue
+++ web/server/vue-cli/src/components/Statistics/Guideline/GuidelineStatistics.vue
@@ -445,7 +445,7 @@ async function fetchStatistics() {
 
   await getAllGuidelineRules();
 
-  const filter = new ReportFilter(baseStatistics.reportFilter);
+  const filter = new ReportFilter(baseStatistics.reportFilter.value);
 
   const checker_stat_result = await new Promise(resolve => {
     ccService.getClient().getCheckerStatusVerificationDetails(
