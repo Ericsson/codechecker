7af842bb1bb99af87182a714127921c6adf5ea0e
Gyorgy Andorka <gyorgy.andorka@ericsson.com>
2026-08-18 17:30:41 +0200

    [cmd] Include any review status for sarif export by default

--- analyzer/codechecker_analyzer/cli/parse.py
+++ analyzer/codechecker_analyzer/cli/parse.py
@@ -207,9 +207,11 @@ def add_arguments_to_parser(parser):
                         dest="review_status",
                         metavar='REVIEW_STATUS',
                         choices=REVIEW_STATUS_VALUES,
-                        default=["confirmed", "unreviewed"],
+                        default=argparse.SUPPRESS,
                         help="Filter results by review statuses. Valid "
-                             f"values are: {', '.join(REVIEW_STATUS_VALUES)}")
+                             f"values are: {', '.join(REVIEW_STATUS_VALUES)}"
+                             " (default: everything for SARIF export,"
+                             " otherwise ['confirmed', 'unreviewed'])")
 
     parser.add_argument('--status',
                         dest="status",
@@ -525,6 +527,11 @@ def main(args):
                      getattr(args, 'output_path', None))
         return
 
+    if 'review_status' not in args:
+        if args.export == 'sarif':
+            args.review_status = REVIEW_STATUS_VALUES
+        else:
+            args.review_status = ["confirmed", "unreviewed"]
     src_comment_status_filter = args.review_status
 
     suppr_handler = None
